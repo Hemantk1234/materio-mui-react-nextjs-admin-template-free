904dccaa22b91543969d1ac193437e7bf51e3085
Hemant Kumbhalkar <hemantkumbhalkar5@gmail.com>
2023-05-19 11:24:17 +0530

    modify from yarn to npm

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "484aeaae990095f3"; }
+/******/ 		__webpack_require__.h = function() { return "bd7ddbca4bedbf79"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.f300f7ac9fe98fd4.hot-update.js
+++ .next/static/webpack/webpack.f300f7ac9fe98fd4.hot-update.js
@@ -33,7 +33,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "484aeaae990095f3"; }
+/******/ 	__webpack_require__.h = function() { return "bd7ddbca4bedbf79"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
